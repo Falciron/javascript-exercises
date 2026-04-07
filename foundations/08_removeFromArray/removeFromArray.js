e6be5a2a7803db6682902f718cef1aec1bfb0641
Falciron <2159742+Falciron@users.noreply.github.com>
2026-04-06 22:12:38 -0700

    Locate and remove all secondary arguments
    Fixes the first two failing tests in
    08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,7 +1,13 @@
-const removeFromArray = function(arr, otherArg) {
-    const foundAtIndex = arr.indexOf(otherArg);
-    arr.splice(foundAtIndex,1);
-    return arr;
+const removeFromArray = function() {
+    const workingArray = arguments[0];
+
+    for (let i = 1; i < arguments.length; i++){
+        const searchTerm = arguments[i];
+        const foundAtIndex = workingArray.indexOf(searchTerm);
+        workingArray.splice(foundAtIndex,1);
+    }
+    
+    return workingArray;
 };
 
 // Do not edit below this line

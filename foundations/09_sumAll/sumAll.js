bef0ef996a42810f66951dce10ccf720df450071
Falciron <2159742+Falciron@users.noreply.github.com>
2026-04-07 12:26:57 -0700

    Sum all numbers between two integers
    Fixes the failing tests in 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    if (int1 < 0 || int2 < 0 || !Number.isInteger(int1) || !Number.isInteger(int2)){
+        return "ERROR";
+    }
 
+    const startInt = Math.min(int1,int2);
+    const endInt = Math.max(int1,int2);
+
+    let runningTotal = 0;
+    for (let currentInt = startInt; currentInt <= endInt; currentInt++){
+        runningTotal += currentInt;
+    }
+
+    return runningTotal;
 };
 
 // Do not edit below this line

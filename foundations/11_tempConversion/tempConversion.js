643480f7ca05037a54ccdc9fe9e9123038a8899d
Falciron <2159742+Falciron@users.noreply.github.com>
2026-04-07 12:51:21 -0700

    Convert temp bidirectionally between F and C
    Fixes the failing tests in 11_tempConversions

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  const celsiusTemp = (fahrenheitTemp - 32) / (9/5);
+  return Number.isInteger(celsiusTemp) ? celsiusTemp : +celsiusTemp.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  const fahrenheitTemp = celsiusTemp * (9/5) + 32;
+  return Number.isInteger(fahrenheitTemp) ? fahrenheitTemp : +fahrenheitTemp.toFixed(1);
 };
 
 // Do not edit below this line

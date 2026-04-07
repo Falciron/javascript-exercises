488241d9e3393a7487b9f3b0a16dc484110dbb68
Falciron <2159742+Falciron@users.noreply.github.com>
2026-04-06 21:33:16 -0700

    Reverse a given starting
    Fixes the failing tests in 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const charArray = str.split("");
+    let reversedString = "";
+    for (const char of charArray){
+        reversedString = char + reversedString;
+    }
 
+    return reversedString;
 };
 
 // Do not edit below this line

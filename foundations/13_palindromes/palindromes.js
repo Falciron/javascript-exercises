c1052c2f53b59d4413c5c961a2835cf950849e8e
Falciron <2159742+Falciron@users.noreply.github.com>
2026-04-12 17:06:47 -0700

    Detect basic alphanumeric palindromes
    Fixes the first two failing tests in
    13_palindromes.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const wordLength = word.length;
+    const lettersToCheck = Math.floor(wordLength/2);
+    const wordArray = word.split("");
 
+    for (let i = 0; i < lettersToCheck; i++){
+        if (wordArray[i] !== wordArray[wordLength - 1 - i]) return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line

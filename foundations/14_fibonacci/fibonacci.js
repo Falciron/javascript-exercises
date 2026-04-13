36b63a58ba0782edd91931f289545a2b6073da5c
Falciron <2159742+Falciron@users.noreply.github.com>
2026-04-12 17:39:18 -0700

    Implement basic fibonacci sequence calculation
    Fixes the first five failing tests in 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const fibArray = [];
+    for (let i = 0; i < num; i++){
+        if (i === 0 || i === 1){
+            fibArray.push(1);
+        } else {
+            fibArray.push(fibArray[i-2] + fibArray[i-1]);
+        }
+    }
 
+    return fibArray.pop();
 };
 
 // Do not edit below this line

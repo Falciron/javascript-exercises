1e45f653622d567d6faa1694b46df9ea291ffe40
Falciron <2159742+Falciron@users.noreply.github.com>
2026-04-12 22:51:18 -0700

    Find the seniorest citizen
    Fixes all failing tests in 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    const currentYear = new Date().getUTCFullYear();
 
+    return peopleArray.reduce((oldestPerson,currentPerson) => {
+        if (oldestPerson === null){
+            return currentPerson;
+        } else {
+            const oldestPersonMaxYear = ("yearOfDeath" in oldestPerson) ? oldestPerson.yearOfDeath : currentYear;
+            const oldestPersonAge = oldestPersonMaxYear - oldestPerson.yearOfBirth;
+            
+            const currentPersonMaxYear = ("yearOfDeath" in currentPerson) ? currentPerson.yearOfDeath : currentYear;
+            const currentPersonAge = currentPersonMaxYear - currentPerson.yearOfBirth;
+
+            return (currentPersonAge > oldestPersonAge) ? currentPerson : oldestPerson;
+        }
+    });
 };
 
 // Do not edit below this line
